94342ee0623c46288f860b74676f45dbc6cabfa3
meliketomakinoglu <140446315+meliketomakinoglu@users.noreply.github.com>
2024-02-13 00:17:51 +0300

    Add menu toggle

---
+++ app.js
@@ -0,0 +1,6 @@
+const bentoMenu = document.querySelector(".circles")
+const socials = document.querySelector(".socials")
+
+bentoMenu.addEventListener("click", () => {
+  socials.classList.toggle("open")
+})
